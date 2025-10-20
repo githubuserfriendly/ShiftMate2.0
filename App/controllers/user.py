cd75d723e73fb32716213f481091695034b527fd
AdaraAli-Khan <alikhanadara@gmail.com>
2025-10-20 02:37:07 +0000

    first commit

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,37 +1,9 @@
+
 from App.models import User, Shift, Attendance
 from App.database import db
 from datetime import datetime, date, timedelta, time as dtime
 from sqlalchemy import and_, or_
 
-def create_user(username, password, isAdmin=False):
-    newuser = User(username=username, password=password, isAdmin=isAdmin)
-    db.session.add(newuser)
-    db.session.commit()
-    return newuser
-
-def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
-
-def get_user(id):
-    return User.query.get(id)
-
-def get_all_users():
-    return User.query.all()
-
-def get_all_users_json():
-    users = User.query.all()
-    if not users:
-        return []
-    users = [user.get_json() for user in users]
-    return users
-
-def update_user(id, username):
-    user = get_user(id)
-    if user:
-        user.username = username
-        db.session.add(user)
-        return db.session.commit()
-    return None
 
 
 def schedule_shift(user_id: int, work_date: date, start: dtime, end: dtime, role=None, location=None):
